ea014320d35c84a65a67c813150ff9ba026b9cd3
Dmytro <dziurziadmytro@gmail.com>
2025-02-20 17:42:05 +0100

    add jobs microservice & add protoc

---
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    ConfigModule,
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
