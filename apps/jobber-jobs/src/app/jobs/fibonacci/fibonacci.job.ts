877c7919e12cb9087673eb4b6989379b00ba0f69
Dmytro <dziurziadmytro@gmail.com>
2025-02-21 15:15:38 +0100

    add pulsar

---
+++ apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
@@ -0,0 +1,14 @@
+import { PulsarClient } from '@jobber/pulsar';
+import { Job } from '../../decorators/job.decorator';
+import { AbstractJob } from '../abstract.job';
+import { FibonacciData } from './fibonacci-data.interface';
+
+@Job({
+  name: 'Fibonacci Job',
+  description: 'Calculate the Fibonacci sequence and store it in the DB',
+})
+export class FibonacciJob extends AbstractJob<FibonacciData> {
+  constructor(pulsarClient: PulsarClient) {
+    super(pulsarClient);
+  }
+}
