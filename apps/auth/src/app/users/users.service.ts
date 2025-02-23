ccbc8c5b7c915580ac377120725443566eb77c6d
Dmytro <dziurziadmytro@gmail.com>
2025-02-23 12:38:37 +0100

    folders refactor & add npm workspaces

---
+++ apps/auth/src/app/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma-clients/auth';
+import { PrismaService } from '../prisma/prisma.service';
+import { hash } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async createUser(createUserInput: Prisma.UserCreateInput) {
+    return this.prismaService.user.create({
+      data: {
+        ...createUserInput,
+        password: await hash(createUserInput.password, 10),
+      },
+    });
+  }
+  async getUsers() {
+    return this.prismaService.user.findMany();
+  }
+
+  async getUser(args: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.findUniqueOrThrow({ where: args });
+  }
+}
